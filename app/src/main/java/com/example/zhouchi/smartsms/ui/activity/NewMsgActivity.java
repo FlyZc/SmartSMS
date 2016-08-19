32bee10b5adb6a6f121d1b3d16c96c6b492e2436
zhouchi <zhouchi0106@gmail.com>
2016-08-19 22:18:53 +0800

    add the activity of new message

---
+++ app/src/main/java/com/example/zhouchi/smartsms/ui/activity/NewMsgActivity.java
@@ -0,0 +1,30 @@
+package com.example.zhouchi.smartsms.ui.activity;
+
+import android.view.View;
+
+import com.example.zhouchi.smartsms.base.BaseActivity;
+
+/**
+ * Created by zhouchi on 2016/8/19.
+ */
+public class NewMsgActivity extends BaseActivity {
+    @Override
+    public void initView() {
+
+    }
+
+    @Override
+    public void initListener() {
+
+    }
+
+    @Override
+    public void initData() {
+
+    }
+
+    @Override
+    public void processClick(View v) {
+
+    }
+}
