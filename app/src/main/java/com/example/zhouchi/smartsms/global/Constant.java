e9c60bd65534753bf4841754f8a2e81cd1e349cb
zhouchi <zhouchi0106@gmail.com>
2016-08-20 22:38:02 +0800

    add some uri which is about the function of insert the data into the database

--- app/src/main/java/com/example/zhouchi/smartsms/global/Constant.java
+++ app/src/main/java/com/example/zhouchi/smartsms/global/Constant.java
@@ -9,6 +9,7 @@ public class Constant {
     public interface URI {
         Uri URI_SMS_CONVERSATION = Uri.parse("content://sms/conversations");
         Uri URI_SMS = Uri.parse("content://sms");
+        Uri URI_GROUP_INSERT = Uri.parse("content://com.zhouchi.smartsms/groups/insert");
     }
     public interface SMS {
         int TYPE_RECEIVE = 1;
